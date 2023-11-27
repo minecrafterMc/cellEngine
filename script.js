7e830af44428635a5c8e15c2793b3f34ac45774d
szymkor11@gmail.com <szymkor11@gmail.com>
2023-11-27 16:47:23 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 ctx.font = "12px Arial";
-const borderColor = "grey";
-const outlineColor = "black";
-const BackgroundColor = "red";
-const cellHeight = 25;
-const cellWidth = 30;
-const BorederSize = 5;
+const BorderColor = "grey";
+const OutlineColor = "blue";
+const BackgroundColor = "cyan";
+const CellHeight = 25;
+const CellWidth = 10;
+const BorderSize = 20;
 const OutlineSize = 5;
-const Columns = 6;
-const Rows = 5;
-const canvasHeight = 2 * OutlineSize + (Rows - 1) * BorederSize + Rows * cellHeight;
-const canvasWidth = 2 * OutlineSize + (Columns - 1) * BorederSize + Columns * cellWidth;
-document.getElementById("canvas").style.width = canvasWidth + "px";
-document.getElementById("canvas").style.height = canvasHeight + "px";
+const Columns = 7;
+const Rows = 20;
+const CanvasHeight = 2 * OutlineSize + (Rows - 1) * BorderSize + Rows * CellHeight;
+const CanvasWidth = 2 * OutlineSize + (Columns - 1) * BorderSize + Columns * CellWidth;
+document.getElementById("canvas").width = CanvasWidth;
+document.getElementById("canvas").height = CanvasHeight;
 
 function setup()
 {
-    let i = 0;
+    let i = OutlineSize + CellWidth;
     let a = 1;
-    ctx.fillStyle = outlineColor;
-    ctx.fillRect(0,0,canvasWidth, canvasHeight);
+    ctx.fillStyle = OutlineColor;
+    ctx.fillRect(0,0,CanvasWidth, CanvasHeight);
     ctx.fillStyle = BackgroundColor;
-    ctx.fillRect(OutlineSize, OutlineSize, OutlineSize, canvasHeight - (OutlineSize * 7));
-    /*
-    while (a != 12)
+    ctx.fillRect(OutlineSize, OutlineSize, CanvasWidth - OutlineSize * 2,CanvasHeight - (OutlineSize * 2));
+    ctx.fillStyle = BorderColor;
+    while (a != Rows + 2)
     {
-        ctx.fillRect(i, 0, 5, 605);
-        i += 30;
+        ctx.fillRect(OutlineSize, OutlineSize, OutlineSize, CanvasWidth - (OutlineSize * 2));
+        i += CellWidth + BorderSize;
         a += 1;
     }
         i = 0;
         a = 1;
+        /*
     while (a != 22)
     {
         ctx.fillRect(0,i, 305, 5);
